8d49c756f9ee89b31b84779f59a42a11bcf27bd8
wocsor <kevinroscom94@gmail.com>
2020-04-16 12:27:43 -0400

    add special case messages again

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -166,6 +166,19 @@ class CarController():
 
     for (addr, ecu, cars, bus, fr_step, vl) in STATIC_MSGS:
       if frame % fr_step == 0 and ecu in self.fake_ecus and self.car_fingerprint in cars:
+
+        # special cases
+        if fr_step == 5 and ecu == Ecu.fwdCamera and bus == 1:
+          cnt = (((frame / 5) % 7) + 1) << 5
+          vl = chr(cnt) + vl
+        elif addr in (0x489, 0x48a) and bus == 0:
+          # add counter for those 2 messages (last 4 bits)
+          cnt = ((frame/100)%0xf) + 1
+          if addr == 0x48a:
+            # 0x48a has a 8 preceding the counter
+            cnt += 1 << 7
+          vl += chr(cnt)
+
         can_sends.append(make_can_msg(addr, vl, bus))
 
     return can_sends
