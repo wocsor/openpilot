237f3d9b91a6b6871db6a8da5f5650a97864b5c3
Bugsy <30946888+bugsy924@users.noreply.github.com>
2020-04-17 12:15:58 +1000

    add UNUSED(to_send);

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -125,7 +125,8 @@ static int gm_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 //     block all commands that produce actuation
 
 static int gm_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
-
+  
+  UNUSED(to_send);
   int tx = 1;
   /*
   int addr = GET_ADDR(to_send);
